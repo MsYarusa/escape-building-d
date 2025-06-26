9b60f23ff28a4045b2ee8fef7e2b2ebcc94d2fd6
AllenP666 <pavelalin30@gmail.com>
2025-06-26 18:19:27 +1000

    new menu system

--- scr/game/main.py
+++ scr/game/main.py
@@ -1,46 +1,62 @@
 import pygame as pg
 
-from game.screens import show_start_screen, show_end_screen, show_dead_screen, show_main_screen, show_level_select_screen, show_level_editor_screen
+from game.screens import (
+    show_start_screen, show_end_screen, show_dead_screen, show_main_screen,
+    show_level_select_screen, show_level_editor_screen, show_settings_screen
+)
 from game.settings import WIN_SIZE, BLACK
-
 
 if __name__ == "__main__":
     pg.init()
 
-    running = True
+    # Словарь со всеми экранами для удобства
+    screens = {
+        'start': show_start_screen,
+        'main': show_main_screen,
+        'dead': show_dead_screen,
+        'end': show_end_screen,
+        'level_select': show_level_select_screen,
+        'level_editor': show_level_editor_screen,
+        'settings': show_settings_screen,
+    }
 
     active_screen_name = 'start'
+    active_screen_params = {}  # Словарь для параметров
 
-    def set_active_screen(screen_name):
-        global active_screen_name
-        active_screen_name = screen_name
+
+    def set_active_screen(screen_data):
+        # ИЗМЕНЕНИЕ: Используем 'global' для доступа к переменным уровня модуля
+        global active_screen_name, active_screen_params
+
+        if isinstance(screen_data, tuple):
+            # Если передан кортеж, например ('settings', {'source_screen': 'start'})
+            active_screen_name = screen_data[0]
+            active_screen_params = screen_data[1]
+        else:
+            # Если передана просто строка
+            active_screen_name = screen_data
+            active_screen_params = {}
+
 
     pg.display.set_caption('Лабиринт')
     screen = pg.display.set_mode(WIN_SIZE)
     clock = pg.time.Clock()
 
+    running = True
     while running:
-
-        current_screen_function = None
-
-        if active_screen_name == 'start':
-            current_screen_function = show_start_screen
-        elif active_screen_name == 'main':
-            current_screen_function = show_main_screen
-        elif active_screen_name == 'dead':
-            current_screen_function = show_dead_screen
-        elif active_screen_name == 'end':
-            current_screen_function = show_end_screen
-        elif active_screen_name == 'level_select':
-            current_screen_function = show_level_select_screen
-        elif active_screen_name == 'level_editor':
-            current_screen_function = show_level_editor_screen
-
-        if current_screen_function:
+        if active_screen_name in screens:
+            current_screen_function = screens[active_screen_name]
             screen.fill(BLACK)
 
-            should_continue_game = current_screen_function(set_active_screen, screen, clock)
+            # Вызываем функцию экрана, передавая ей параметры через **
+            should_continue_game = current_screen_function(
+                set_active_screen, screen, clock, **active_screen_params # Не трогать!
+            )
 
             if not should_continue_game:
                 running = False
+        else:
+            print(f"Ошибка: Экран '{active_screen_name}' не найден!")
+            running = False
 
+    pg.quit()
---
+++ scr/game/screens/settings_screen.py
@@ -0,0 +1,77 @@
+import pygame as pg
+from game.settings import WIN_WIDTH, WIN_HEIGHT, BLACK
+from game.ui import Button
+from game.ui.slider import Slider
+from game.settings_manager import settings_manager
+from game.utils.audio_manager import update_volumes
+
+
+def show_settings_screen(set_active_screen, screen, clock, source_screen='start'):
+    # Если мы в режиме паузы, рисуем игровой экран на фоне
+    if source_screen == 'pause':
+        static_background = screen.copy()
+    else:
+        static_background = None
+
+    overlay = pg.Surface((WIN_WIDTH, WIN_HEIGHT))
+    overlay.fill(BLACK)
+    overlay.set_alpha(200)
+
+    font_title = pg.font.SysFont('calibry', 56)
+    font_label = pg.font.SysFont('calibry', 36)
+    title = font_title.render('Настройки', True, (255, 255, 255))
+    title_rect = title.get_rect(center=(WIN_WIDTH // 2, 100))
+
+    music_label = font_label.render('Громкость музыки', True, (255, 255, 255))
+    sfx_label = font_label.render('Громкость эффектов', True, (255, 255, 255))
+
+    slider_width = 400
+    music_slider = Slider((WIN_WIDTH // 2 - slider_width // 2, 220), (slider_width, 20),
+                          settings_manager.music_volume, 0, 1)
+    sfx_slider = Slider((WIN_WIDTH // 2 - slider_width // 2, 320), (slider_width, 20),
+                        settings_manager.sfx_volume, 0, 1)
+
+    back_btn = Button((WIN_WIDTH // 2 - 150, 450), 'btn')
+    back_btn.scale((300, 70))
+    back_btn.set_text('Назад', font_size=36)
+
+    running = True
+    while running:
+        clock.tick(60)
+
+        # Обновляем громкость в реальном времени
+        if music_slider.val != settings_manager.music_volume or sfx_slider.val != settings_manager.sfx_volume:
+            settings_manager.music_volume = music_slider.val
+            settings_manager.sfx_volume = sfx_slider.val
+            update_volumes()
+
+        if static_background:
+            screen.blit(static_background, (0, 0))
+
+        screen.blit(overlay, (0, 0))
+        screen.blit(title, title_rect)
+        screen.blit(music_label, (music_slider.pos[0], music_slider.pos[1] - 45))
+        screen.blit(sfx_label, (sfx_slider.pos[0], sfx_slider.pos[1] - 45))
+        music_slider.draw(screen)
+        sfx_slider.draw(screen)
+        screen.blit(back_btn.image, back_btn.rect)
+
+        pg.display.flip()
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                settings_manager.save_settings()
+                return False
+
+            music_slider.handle_event(event)
+            sfx_slider.handle_event(event)
+
+            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+                if back_btn.pressed(event.pos):
+                    settings_manager.save_settings()
+                    if source_screen == 'pause':
+                        return True  # Возвращаемся в меню паузы
+                    else:
+                        set_active_screen(source_screen)
+                        return True  # Возвращаемся в главное меню
+    return True
